bf9c2bd019e3609add103b567ff0335405c925aa
Arindam Chakraborty <arindamckbt@gmail.com>
2024-03-07 00:35:00 +0530

    Speaker animation removed

--- src/Components/Speakers.jsx
+++ src/Components/Speakers.jsx
@@ -24,16 +24,16 @@ const Speakers = () => {
         <h2 data-aos="fade-in"className="text-[22px] lg:px-10 py:5">Plenary Speaker ((11 to 13)<sup>th</sup> March 2023):</h2>
         <ul>
         <img src={img1} width={100} height={250} className='mb-0 border-spacing-1 border-blue-500 rounded-lg shadow-md mx-4' alt="speaker"/>
-          <li data-aos="fade-in"className="text-[18px] lg:px-5 text-gray-600 font-bold">Dr. Saikat Ranjan Maity</li>
-          <div data-aos="fade-in"className="text-[16px] lg:px-10 text-gray-600 font-semibold">
+          <li className="text-[18px] lg:px-5 text-gray-600 font-bold">Dr. Saikat Ranjan Maity</li>
+          <div className="text-[16px] lg:px-10 text-gray-600 font-semibold">
             Tittle:
-            <p data-aos="fade-in"className="text-[14px] lg:px-10 text-gray-600 font-bold">
+            <p className="text-[14px] lg:px-10 text-gray-600 font-bold">
             Laser-aided direct metal deposition
             </p>
           </div>
-          <div data-aos="fade-in"className="text-[16px] lg:px-10 text-gray-600 font-semibold">
+          <div className="text-[16px] lg:px-10 text-gray-600 font-semibold">
           Designation:
-            <p data-aos="fade-in"className="text-[14px] lg:px-10 text-gray-600 font-bold">
+            <p className="text-[14px] lg:px-10 text-gray-600 font-bold">
             Associate Professor, Department of Mechanical Engineering, National Institute of Technology Silchar
             </p>
           </div>
@@ -41,16 +41,16 @@ const Speakers = () => {
 
         <ul>
         <img src={img2} width={100} height={250} className='mb-0 border-spacing-1 border-blue-500 rounded-lg shadow-md mx-4' alt="speaker"/>
-          <li data-aos="fade-in"className="text-[18px] lg:px-5 text-gray-600 font-bold">Prof Gouranga Lal Datta</li>
-          <div data-aos="fade-in"className="text-[16px] lg:px-10 text-gray-600 font-semibold">
+          <li className="text-[18px] lg:px-5 text-gray-600 font-bold">Prof Gouranga Lal Datta</li>
+          <div className="text-[16px] lg:px-10 text-gray-600 font-semibold">
             Tittle:
-            <p data-aos="fade-in"className="text-[14px] lg:px-10 text-gray-600 font-bold">
+            <p className="text-[14px] lg:px-10 text-gray-600 font-bold">
             Foundry Technology
             </p>
           </div>
-          <div data-aos="fade-in"className="text-[16px] lg:px-10 text-gray-600 font-semibold">
+          <div className="text-[16px] lg:px-10 text-gray-600 font-semibold">
           Designation:
-            <p data-aos="fade-in"className="text-[14px] lg:px-10 text-gray-600 font-bold">
+            <p className="text-[14px] lg:px-10 text-gray-600 font-bold">
             Retired Dean, IIT Kharagpur 
             </p>
           </div>
@@ -58,16 +58,16 @@ const Speakers = () => {
 
         <ul>
         <img src={img4} width={100} height={250} className='mb-0 border-spacing-1 border-blue-500 rounded-lg shadow-md mx-4' alt="speaker"/>
-          <li data-aos="fade-in"className="text-[18px] lg:px-5 text-gray-600 font-bold">Dr. Biswanath Doloi</li>
-          <div data-aos="fade-in"className="text-[16px] lg:px-10 text-gray-600 font-semibold">
+          <li className="text-[18px] lg:px-5 text-gray-600 font-bold">Dr. Biswanath Doloi</li>
+          <div className="text-[16px] lg:px-10 text-gray-600 font-semibold">
             Tittle:
-            <p data-aos="fade-in"className="text-[14px] lg:px-10 text-gray-600 font-bold">
+            <p className="text-[14px] lg:px-10 text-gray-600 font-bold">
             An overview on laser-based manufacturing
             </p>
           </div>
-          <div data-aos="fade-in"className="text-[16px] lg:px-10 text-gray-600 font-semibold">
+          <div className="text-[16px] lg:px-10 text-gray-600 font-semibold">
           Designation:
-            <p data-aos="fade-in"className="text-[14px] lg:px-10 text-gray-600 font-bold">
+            <p className="text-[14px] lg:px-10 text-gray-600 font-bold">
             Professor, Department of Production Engineering, Jadavpur University 
             </p>
           </div>
@@ -75,16 +75,16 @@ const Speakers = () => {
 
         <ul>
         <img src={img3} width={100} height={250} className='mb-0 border-spacing-1 border-blue-500 rounded-lg shadow-md mx-4' alt="speaker"/>
-          <li data-aos="fade-in"className="text-[18px] lg:px-5 text-gray-600 font-bold">Dr. Nilrudra Mandal</li>
-          <div data-aos="fade-in"className="text-[16px] lg:px-10 text-gray-600 font-semibold">
+          <li className="text-[18px] lg:px-5 text-gray-600 font-bold">Dr. Nilrudra Mandal</li>
+          <div className="text-[16px] lg:px-10 text-gray-600 font-semibold">
             Tittle:
-            <p data-aos="fade-in"className="text-[14px] lg:px-10 text-gray-600 font-bold">
+            <p className="text-[14px] lg:px-10 text-gray-600 font-bold">
             Coatings & Surface Engineering 
             </p>
           </div>
-          <div data-aos="fade-in"className="text-[16px] lg:px-10 text-gray-600 font-semibold">
+          <div className="text-[16px] lg:px-10 text-gray-600 font-semibold">
           Designation:
-            <p data-aos="fade-in"className="text-[14px] lg:px-10 text-gray-600 font-bold">
+            <p className="text-[14px] lg:px-10 text-gray-600 font-bold">
             Senior Principal Scientist and Heading the Coatings & Surface Engineering Group at CSIR-CMERI, Durgapur
             </p>
           </div>
